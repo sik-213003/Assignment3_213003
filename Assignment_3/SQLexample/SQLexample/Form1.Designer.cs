e423be9007125246d8b80e4d616d9154123394bb
sik-213003 <145955613+sik-213003@users.noreply.github.com>
2023-11-30 22:16:38 +0500

    Initial Commit

---
+++ Assignment_3/SQLexample/SQLexample/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace SQLexample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.unitPriceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantityInStockDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableProductBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productDBDataSet = new SQLexample.ProductDBDataSet();
+            this.table_ProductTableAdapter = new SQLexample.ProductDBDataSetTableAdapters.Table_ProductTableAdapter();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableProductBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productDBDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Insert a Record";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(122, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Delete a Record";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(609, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(211, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "View All Updated Records";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.unitPriceDataGridViewTextBoxColumn,
+            this.quantityInStockDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tableProductBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 154);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(958, 303);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // unitPriceDataGridViewTextBoxColumn
+            // 
+            this.unitPriceDataGridViewTextBoxColumn.DataPropertyName = "UnitPrice";
+            this.unitPriceDataGridViewTextBoxColumn.HeaderText = "UnitPrice";
+            this.unitPriceDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.unitPriceDataGridViewTextBoxColumn.Name = "unitPriceDataGridViewTextBoxColumn";
+            this.unitPriceDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // quantityInStockDataGridViewTextBoxColumn
+            // 
+            this.quantityInStockDataGridViewTextBoxColumn.DataPropertyName = "QuantityInStock";
+            this.quantityInStockDataGridViewTextBoxColumn.HeaderText = "QuantityInStock";
+            this.quantityInStockDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.quantityInStockDataGridViewTextBoxColumn.Name = "quantityInStockDataGridViewTextBoxColumn";
+            this.quantityInStockDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // tableProductBindingSource
+            // 
+            this.tableProductBindingSource.DataMember = "Table_Product";
+            this.tableProductBindingSource.DataSource = this.productDBDataSet;
+            // 
+            // productDBDataSet
+            // 
+            this.productDBDataSet.DataSetName = "ProductDBDataSet";
+            this.productDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // table_ProductTableAdapter
+            // 
+            this.table_ProductTableAdapter.ClearBeforeFill = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(445, 87);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 36);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Update a Record";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 520);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableProductBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productDBDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private ProductDBDataSet productDBDataSet;
+        private System.Windows.Forms.BindingSource tableProductBindingSource;
+        private ProductDBDataSetTableAdapters.Table_ProductTableAdapter table_ProductTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn unitPriceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantityInStockDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Assignment_3/SQLexample/SQLexample/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLexample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlCommand cmd = new SqlCommand();
+        string connectionString = "Data Source=DESKTOP-ERUUV97\\SQLEXPRESS;Initial Catalog=ProductDB;Integrated Security=True";
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = true;
+            
+            // TODO: This line of code loads data into the 'productDBDataSet.Table_Product' table. You can move, or remove it, as needed.
+            this.table_ProductTableAdapter.Fill(this.productDBDataSet.Table_Product);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                cmd.Connection = connection;
+                connection.Open();
+
+                cmd.CommandText = "INSERT INTO Table_Product (Name, UnitPrice, QuantityInStock) VALUES ('Pins', 5, 500)";
+                int totalRowsAffected = cmd.ExecuteNonQuery();
+                MessageBox.Show("Total Rows Inserted = " + totalRowsAffected, "Info", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                connection.Close();
+            }   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                cmd.Connection = connection;
+                connection.Open();
+
+                cmd.CommandText = "DELETE FROM Table_Product WHERE Name = 'Pins'";
+                int totalRowsAffected = (int)cmd.ExecuteNonQuery();
+                MessageBox.Show("Total Rows Deleted = " + totalRowsAffected, "Info", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                cmd.Connection = connection;
+                connection.Open();
+
+                string Query = "SELECT * FROM Table_Product";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(Query, connectionString);
+                DataSet set = new DataSet();
+
+                adapter.Fill(set, "Table_Product");
+                dataGridView1.DataSource = set.Tables["Table_Product"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                cmd.Connection = connection;
+                connection.Open();
+
+                cmd.CommandText = "UPDATE Table_Product SET QuantityInStock = 0 WHERE Name = 'Pencil'";
+                int totalRowsAffected = (int)cmd.ExecuteNonQuery();
+                MessageBox.Show("Total Rows Updated = " + totalRowsAffected, "Info", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
